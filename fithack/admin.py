71f3eb5ccfb61d272c6943818e1293ebab9be9de
Johanna <jweinsziehr@gmx.de>
2014-11-08 19:41:11 -0800

    register models at admin

--- fithack/admin.py
+++ fithack/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from models import Member, Group, GroupAdmin, Data
 
-# Register your models here.
+admin.site.register(Member)
+admin.site.register(Group)
+admin.site.register(GroupAdmin)
+admin.site.register(Data)
