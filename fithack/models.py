8d24564115b7765e3b410138020fd5864c596e07
Johanna <jweinsziehr@gmx.de>
2014-11-08 18:46:52 -0800

    adding goal

--- fithack/models.py
+++ fithack/models.py
@@ -20,6 +20,7 @@ class Group(models.Model):
     name = models.CharField(max_length=200)
     start_date = models.DateField()
     end_date = models.DateField()
+    goal = models.FloatField()
 
 class GroupAdmin(models.Model):
     admin = models.BooleanField(default=False)
