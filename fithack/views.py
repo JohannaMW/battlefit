22049a72115eba3ed66bc7789e9b770ddc642eac
j3dev <j3pydev@gmail.com>
2014-11-08 21:14:57 -0800

    merging with develop branch

--- fithack/views.py
+++ fithack/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from fithack.forms import GroupForm
+from fithack.models import Group, Data
 
 
 def create_group(request):
@@ -7,8 +8,28 @@ def create_group(request):
         form = GroupForm(request.POST)
         if form.is_valid():
             if form.save():
-                return redirect("/epochs")
+                return redirect("/create")
     else:
         form = GroupForm()
     data = {'form': form}
-    return render(request, "Epoch/new_epoch.html", data)
+    return render(request, "create_group.html", data)
+
+def group(request, group_id):
+    group = Group.objects.get(id=group_id)
+    data = Data.objects.get(member = request.user, date__range=["2011-01-01", "2011-01-31"])
+    if group.category == "W":
+        data_group = data.calories_burned
+        data_w = sum(data_group)/len(data_group)
+        score = group.goal - data_w / group.goal
+    elif group.category == "H":
+        data_group = data.calories_consumed
+        data_h = sum(data_group)/len(data_group)
+        score = group.goal - data_h / group.goal
+    else:
+        data_group = data.body_fat
+        data_f = sum(data_group)/len(data_group)
+        score = group.goal - data_f / group.goal
+
+
+
+
